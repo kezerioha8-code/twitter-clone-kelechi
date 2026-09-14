9c21213c23f838021440d0e68d12d495fa62c700
Flourish Oluwatimilehin <126780643+davidflourish@users.noreply.github.com>
2026-09-14 12:14:31 +0100

    Add files via upload

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,44 +1,40 @@
- import "./profile.css";
- import Sidebar from "../../component/sidebar/Sidebar";
- import Topbar from "../../component/topbar/Topbar";
- import Feed from "../../component/feed/Feed";
-import Rightbar from "../../component/rightbar/Rightbar";
+import "./profile.css";
+import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
 
 export default function Profile() {
-    return (
-        <>
-        <Topbar />
-        <div className="profile">
+  return (
+    <>
+      <Topbar />
+      <div className="profile">
         <Sidebar />
         <div className="profileRight">
-        <div className="profileRightTop">
-        <div className="profileCover">
-        <img
-           className="profileCoverImg"
-           src="assets/post/3.jpeg"
-           alt=""
-           />
-           <img
-            className="profileUserImg"
-            src="assests/person/7.jpeg"
-            alt=""
-            />
-            </div> 
-              <div className="profileInfo">
-                <h4 className="profileInfoName">John Doe</h4>
-                <span className="profileInfoDesc">Hello my friends</span>
-                </div>
-                </div>
-                <div className="profileRightBottom">
-                <Feed />
-                <Rightbar profile />
-                </div>
-                </div>
-                </div>
-                </>
-    );
+          <div className="profileRightTop">
+            <div className="profileCover">
+              <img
+                className="profileCoverImg"
+                src="assets/post/3.jpeg"
+                alt=""
+              />
+              <img
+                className="profileUserImg"
+                src="assets/person/7.jpeg"
+                alt=""
+              />
+            </div>
+            <div className="profileInfo">
+                <h4 className="profileInfoName">Safak Kocaoglu</h4>
+                <span className="profileInfoDesc">Hello my friends!</span>
+            </div>
+          </div>
+          <div className="profileRightBottom">
+            <Feed />
+            <Rightbar profile/>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
-        
-
-
-    
